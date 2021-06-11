ccc2e458d928eb86a8b25445f5f3264aed52daff
opencovid-mr <63929979+opencovid-mr@users.noreply.github.com>
2021-06-11 12:38:02 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,6 +5,9 @@ data_path = 'data/latest'
 
 wb = load_workbook(f'{data_path}/data.xlsx', read_only=True)
 
+col=[3,3,3,3,4,3,3,5,5]
+indice=0
+
 # get the report date
 date_list = wb['casi_regioni']['A2'].value.split('/')
 report_date = f'{date_list[2]}-{date_list[1]}-{date_list[0]}'
@@ -14,8 +17,9 @@ with open(f'{data_path}/date', 'w') as f:
     f.write(report_date)
 
 # Write data to file
-for sheet in wb.worksheets:
+for sheet in wb.worksheets[2:]:
     with open(f'{data_path}/{sheet.title}.csv', 'w', newline="") as f:
         c = csv.writer(f)
-        for r in sheet.iter_rows(min_row=1, min_col=1, max_row=1000, max_col=3):
+        for r in sheet.iter_rows(min_row=1, min_col=1, max_row=1000, max_col=col[indice]):
             c.writerow([cell.value for cell in r])
+        indice=indice+1
